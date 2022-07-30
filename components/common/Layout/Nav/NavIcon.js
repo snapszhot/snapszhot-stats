252ffc110902c64a279ce61d3dbeba73aba1aa90
Paul Habeeb <paulhabeeb@hey.com>
2022-07-30 13:54:06 -0400

    Show disabled next/previous buttons

--- components/common/Layout/Nav/NavIcon.js
+++ components/common/Layout/Nav/NavIcon.js
@@ -1,21 +1,32 @@
 import PropTypes from 'prop-types'
 import Link from 'next/link'
+import cn from 'classnames'
+import styles from './NavIcon.module.scss'
 
-export default function NavIcon({ children, link, title }) {
+export default function NavIcon({ children, disabled, link, title }) {
+    const className = cn(styles.icon, {
+        [styles.disabled]: disabled,
+    })
+
     return (
         <li>
-            <Link href={link}>
-                <a>
-                    <span className='visuallyHidden'>{title}</span>
-                    {children}
-                </a>
-            </Link>
+            {disabled ? (
+                <span className={className}>{children}</span>
+            ) : (
+                <Link href={link}>
+                    <a>
+                        <span className='visuallyHidden'>{title}</span>
+                        <span className={className}>{children}</span>
+                    </a>
+                </Link>
+            )}
         </li>
     )
 }
 
 NavIcon.propTypes = {
     children: PropTypes.node,
+    disabled: PropTypes.bool,
     link: PropTypes.string,
     title: PropTypes.string,
 }
